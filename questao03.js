16c446de2aead9f0fc72175315abf092ea361a58
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-09-17 20:53:50 -0300

    criação da questao03

---
+++ questao03.js
@@ -0,0 +1,13 @@
+// Cálculo do Valor de SOMA
+
+const INDICE = 12;
+let SOMA = 0;
+let K = 1;
+
+while (K < INDICE) {
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+// Exibir o valor de SOMA
+console.log(`O valor da variável soma é ${SOMA}`);
